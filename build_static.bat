d041a35039a3a5a35bdf5f15f2522d4cab2dc0d1
Isabella Basso <isabellabdoamaral@gmail.com>
2026-03-05 15:20:54 -0300

    update winbin

--- build_static.bat
+++ build_static.bat
@@ -3,7 +3,7 @@
 setlocal EnableDelayedExpansion
 
 set vendor_dir=freetype
-set binaries_dir=objs\.libs\
+set build_dir=build_static
 
 if not exist %vendor_dir% (
     git clone --recurse-submodules --revision 23b6cd27ff19b70cbf98e058cd2cf0647d5284ff https://github.com/freetype/freetype --depth=1 %vendor_dir%
@@ -11,13 +11,13 @@ if not exist %vendor_dir% (
 pushd %vendor_dir%
 
 echo Configuring build...
-cmake -S . -B build -DFT_DISABLE_ZLIB=TRUE -DFT_DISABLE_PNG=TRUE -DFT_DISABLE_HARFBUZZ=TRUE -DFT_DISABLE_ZLIB=TRUE -DFT_DISABLE_BROTLI=TRUE -DFT_DISABLE_GZIP=TRUE -DCMAKE_BUILD_TYPE=Release
+cmake -S . -B %build_dir% -DFT_DISABLE_ZLIB=TRUE -DFT_DISABLE_PNG=TRUE -DFT_DISABLE_HARFBUZZ=TRUE -DFT_DISABLE_ZLIB=TRUE -DFT_DISABLE_BROTLI=TRUE -DFT_DISABLE_GZIP=TRUE -DCMAKE_BUILD_TYPE=Release -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded
 REM make setup visualc
 
 echo Building project...
-cmake --build build -j%NUMBER_OF_PROCESSORS% --config Release
+cmake --build %build_dir% -j%NUMBER_OF_PROCESSORS% --config Release
 
-copy /y %binaries_dir%\freetype_static.lib ..\
+copy /y %build_dir%\Release\freetype.lib ..\freetype_static.lib
 
 echo Build completed successfully!
 popd
